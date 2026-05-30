4df65563ac3f52b41c5ced83147accc49b6b07e0
Nicolás García <espallemann@gmail.com>
2026-05-30 20:41:33 +0200

    Contacto

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,7 @@ const navLinks = [
   { label: "Informes",  href: "#informes" },
   { label: "Videos",    href: "#videos" },
   { label: "Nosotros",  href: "#nosotros" },
+  { label: "Contacto",  href: "#contacto" },
 ];
 
 function smoothScrollTo(href: string) {
